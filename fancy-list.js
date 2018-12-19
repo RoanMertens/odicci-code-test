e2557ae6d9bc38ff3b0528537dc6e71e71803336
Roan Mertens <roanmertens91@gmail.com>
2018-12-19 21:48:01 +0000

    finished the making the list private

--- fancy-list.js
+++ fancy-list.js
@@ -1,108 +1,115 @@
-
-const _flattenItems = Symbol('flattenItems')
-const _checkType = Symbol('checkType')
-const _filList = Symbol('filList')
-
 class FancyList {
-  constructor(input){
-    this.list = []
-    this.input = input
-    this.type = typeof(this.input[0])
-    this[_flattenItems](this.input)
-    this[_checkType](this.input)
-    this[_filList](this.input)
-  }
+  constructor(...input){
 
-  // public
-
-  // get items
-  getItemAt(index) { return this.list[index] }
-
-  getItemsAt(index, numberOfItems){
-    return this.list.slice(index, numberOfItems += 1)
-  }
-
-  // add items
-  addItem(item){
-    if(this.type != typeof(item)){
-      throw "elements are not all of the same type!";
+    // methods needed during the construction
+    this.addItemsToList = function(items) {
+      this.checkType(items)
+      _list.push(items)
+      _list = _list.flat()
     }
-    this[_filList](item)
+
+    // remove after test fase!
+    this.showList = function() {
+      return _list
+    }
+
+    // variables
+    let _list = []
+    this.items = []
+    this.input = this.flattenItems(input)
+    this.type = typeof(this.input[0])
+    this.addItemsToList(this.input)
+
+    // methods to get items
+    this.getItemAt = function(index) { return _list[index] }
+
+    this.getItemsAt = function(index, numberOfItems) {
+    return _list.slice(index, numberOfItems += 1)
+    }
+
+    // insert items
+    this.insertItemAt = function(index, item) {
+      this.checkType(item)
+      _list.splice(index, 0, item)
+    }
+
+    this.insertItemsAt = function(index, ...items){
+      this.checkType(items)
+      items = this.flattenItems(items)
+      _list.splice(index, 0, items)
+      _list = this.flattenItems(_list)
+    }
+
+    // remove items
+    this.removeItemAt = function(index) {
+      // _list.splice(index, 1)
+      this.removeItemsAt(index, 1)
+    }
+
+    this.removeItemsAt = function(index, numberOfItems){
+      _list.splice(index, numberOfItems)
+    }
+
+    this.removeItem = function(item){
+      const index = _list.indexOf(item)
+      this.removeItemsAt(index, 1)
+    }
+
+    this.removeItems = function(...items){
+      items.forEach(function(item) {
+        const index = _list.indexOf(item)
+        this.removeItemsAt(index, 1)
+      }, this)
+    }
   }
 
-  addItems(...items){
-    items = this[_flattenItems](items)
-    this[_checkType](items)
-    this[_filList](items)
+  // methods to add items
+  addItem(item) {
+    this.checkType(item)
+    this.addItems(item)
+  }
+  addItems(...items) {
+    this.checkType(items)
+    const item = items.flat()
+    this.addItemsToList(item)
   }
 
-  // insert items
-  insertItemAt(index, item){
-    this.list.splice(index, 0, item)
-  }
-
-  insertItemsAt(index, ...items){
-    this.list.splice(index, 0, items)
-    this.list = this[_flattenItems](this.list)
-  }
-
-  // remove items
-  removeItemAt(index){
-    delete this.list[index]
-    this.list = this[_flattenItems](this.list)
-  }
-
-  removeItemsAt(index, numberOfItems){
-    this.list.splice(index, numberOfItems)
-  }
-
-  removeItem(item){
-    const index = this.list.indexOf(item)
-    this.removeItemAt(index)
-  }
-
-  removeItems(...items){
-    items.forEach(function(item) {
-      const index = this.list.indexOf(item)
-      this.removeItemAt(index)
-    }, this)
-  }
-
-  // private
 
   // flattens the arrays
-  [_flattenItems](items) { return items.flat() }
-
-  // checks the datatype of the input
-  [_checkType](items) {
-    items.forEach(function(item) {
-      if(this.type != typeof(item)){
-        throw "elements are not all of the same type!";
-      }
-    }, this)
+  flattenItems(items) {
+    return items.flat()
   }
 
-  // adds items to the list
-  [_filList](item) {
-    this.list.push(item)
-    this.list = this[_flattenItems](this.list)
+  // // checks the datatype of the input
+  checkType(items) {
+    if (Array.isArray(items)) {
+      items.forEach(function(item) {
+        if(this.type != typeof(item)){
+          throw "elements are not all of the same type!";
+        }
+      }, this)
+    } else {
+      if(this.type != typeof(items)){
+        throw "elements are not all of the same type!";
+      }
+    }
   }
 }
 
-const myList1 = new FancyList([3, 4, 7, 9, 9]);
 
-myList1.addItem(1)
-// console.log(myList1.input)
-// console.log(myList1.getItemAt(1))
-// console.log(myList1.getItemsAt(1, 2))
-// console.log(myList1.input)
-// console.log(" ")
 
-// console.log(myList1[_list])
-// myList1.removeItems(3, 9)
-console.log(myList1.list)
 
-// myList1.removeItemsAt(0, 2)
-// myList1.removeItem(5)
-myList1.insertItemsAt(3, 14, 100)
-console.log(myList1.list)
+
+const myList1 = new FancyList([3, 4, 7, 9]);
+myList1.addItem(44)
+myList1.addItems([12, 14])
+
+// myList1.insertItemAt(1, "11")
+myList1.insertItemsAt(1, [11, 55, "123", 5555])
+// myList1.removeItemAt(6)
+// myList1.removeItemsAt(2, 4)
+// myList1.removeItem(5555)
+myList1.removeItems(5555, 55, 9, 44)
+// console.log('getting item at index 5:',myList1.getItemAt(5))
+// console.log('getting three items starting at 2:', myList1.getItemsAt(1, 3))
+console.log(myList1.showList())
